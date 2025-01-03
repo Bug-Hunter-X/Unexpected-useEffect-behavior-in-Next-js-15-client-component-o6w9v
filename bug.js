94f30b2c8448794500d8e0e47050e3b890f20640
BugHunter-X <askzenai@gmail.com>
2025-01-03 11:34:08 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,33 @@
+```javascript
+// pages/index.js
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to my Next.js app</h1>
+      <p>This is a paragraph</p>
+      <MyComponent/>
+    </div>
+  );
+}
+
+// components/MyComponent.js
+
+function MyComponent() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log('Component mounted');
+    return () => {
+      console.log('Component unmounted');
+    };
+  }, []);
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+    </div>
+  );
+}
+```
